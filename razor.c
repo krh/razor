krh/razor

commit 7b26d6b3e64902e6c4ccd764e1b83fc0a4ed14a2

Initalize empty package sets with a proper empty file list.

razor.c
@@ -122,7 +122,19 @@ struct razor_set_section razor_sections[] = {
 struct razor_set *
 razor_set_create(void)
 {
-	return zalloc(sizeof(struct razor_set));
+	struct razor_set *set;
+	struct razor_entry *e;
+
+	set = zalloc(sizeof *set);
+
+	array_init(&set->files);
+	e = array_add(&set->files, sizeof *e);
+	e->name = 0;
+	e->flags = RAZOR_ENTRY_LAST;
+	e->start = 0;
+	list_set_empty(&e->packages);
+
+	return set;
 }
 
 struct razor_set *
@@ -694,9 +706,7 @@ build_file_tree(struct razor_importer *importer)
 	}
 
 	count_entries(&root);
-	array_init(&importer->set->files);
-
-	e = array_add(&importer->set->files, sizeof *e);
+	e = importer->set->files.data;
 	e->name = root.name;
 	e->flags = RAZOR_ENTRY_LAST;
 	e->start = importer->files.size ? 1 : 0;

commit 9fe667f14644825cb97be9f4b8fab8f287326a12

Fix file tree merging (broken since 7b26d6b3)

razor.c
@@ -1608,7 +1608,9 @@ merge_files(struct razor_merger *merger)
 	map1 = merger->source1.file_map;
 	map2 = merger->source2.file_map;
 
-	md.merged = add_file(merger, "");
+	md.merged = 0;
+	root = (struct razor_entry *) merger->set->files.data;
+	root->name = hashtable_tokenize(&merger->table, "");
 
 	if (merger->source1.set->files.size) {
 		root = (struct razor_entry *) merger->source1.set->files.data;